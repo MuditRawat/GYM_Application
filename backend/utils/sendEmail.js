19dbdd864fbca542d41f65d78cb635def0fedcb6
Mudit Rawat <mr930542@gmail.com>
2026-06-23 00:37:40 +0530

    Fix Nodemailer configuration

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,9 +2,7 @@ import nodeMailer from "nodemailer";
 
 export const sendEmail = async (options) => {
   const transporter = nodeMailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    service: process.env.SMTP_SERVICE,
+    service: "gmail",
     auth: {
       user: process.env.SMTP_MAIL,
       pass: process.env.SMTP_PASSWORD,
@@ -15,7 +13,8 @@ export const sendEmail = async (options) => {
     from: process.env.SMTP_MAIL,
     to: options.email,
     subject: options.subject,
-    text: `${options.message} \n\nEmail of User Who Sent The Message: ${options.userEmail}`,
+    text: `${options.message}\n\nEmail of User Who Sent The Message: ${options.userEmail}`,
   };
+
   await transporter.sendMail(mailOptions);
 };
